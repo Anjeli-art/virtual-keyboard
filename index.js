b87c1442ba8afab461392b820b396e05b6f0d59b
Anjeli-art <shinyanj@gmail.com>
2023-05-01 16:26:01 +0300

    feat: add part of the basic functionality adding deleting characters change laguage with storage voice assistant

---
+++ index.js
@@ -0,0 +1,659 @@
+const data = {
+  keybordData: [
+    {
+      shiftRu: "СapsLock",
+      shiftEn: "СapsLock",
+      valueRu: "СapsLock",
+      valueEn: "СapsLock",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Звук &#127925;",
+      shiftEn: "Sound &#127925;",
+      valueRu: "Звук &#127925;",
+      valueEn: "Sound &#127925;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Язык &#127759;",
+      shiftEn: "Lang &#127758;",
+      valueRu: "Язык &#127759;",
+      valueEn: "Lang &#127758;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Помощь &#128075;",
+      shiftEn: "Helper &#128075;",
+      valueRu: "Помощь &#128075;",
+      valueEn: "Helper &#128075;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "BackSpace",
+      shiftEn: "BackSpace",
+      valueRu: "BackSpace",
+      valueEn: "BackSpace",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ё",
+      shiftEn: "~",
+      valueRu: "ё",
+      valueEn: "`",
+      lightMode: false,
+    },
+    {
+      shiftRu: "!",
+      shiftEn: "!",
+      valueRu: "1",
+      valueEn: "1",
+      lightMode: false,
+    },
+    {
+      shiftRu: '"',
+      shiftEn: "@",
+      valueRu: "2",
+      valueEn: "2",
+      lightMode: false,
+    },
+    {
+      shiftRu: "№",
+      shiftEn: "#",
+      valueRu: "3",
+      valueEn: "3",
+      lightMode: false,
+    },
+    {
+      shiftRu: ";",
+      shiftEn: "$",
+      valueRu: "4",
+      valueEn: "4",
+      lightMode: false,
+    },
+    {
+      shiftRu: "%",
+      shiftEn: "%",
+      valueRu: "5",
+      valueEn: "5",
+      lightMode: false,
+    },
+    {
+      shiftRu: ":",
+      shiftEn: "^",
+      valueRu: "6",
+      valueEn: "6",
+      lightMode: false,
+    },
+    {
+      shiftRu: "?",
+      shiftEn: "&",
+      valueRu: "7",
+      valueEn: "7",
+      lightMode: false,
+    },
+    {
+      shiftRu: "*",
+      shiftEn: "*",
+      valueRu: "8",
+      valueEn: "8",
+      lightMode: false,
+    },
+    {
+      shiftRu: "(",
+      shiftEn: "(",
+      valueRu: "9",
+      valueEn: "9",
+      lightMode: false,
+    },
+    {
+      shiftRu: ")",
+      shiftEn: ")",
+      valueRu: "0",
+      valueEn: "0",
+      lightMode: false,
+    },
+    {
+      shiftRu: "_",
+      shiftEn: "_",
+      valueRu: "-",
+      valueEn: "-",
+      lightMode: false,
+    },
+    {
+      shiftRu: "+",
+      shiftEn: "+",
+      valueRu: "=",
+      valueEn: "=",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Й",
+      shiftEn: "Q",
+      valueRu: "й",
+      valueEn: "q",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ц",
+      shiftEn: "W",
+      valueRu: "ц",
+      valueEn: "w",
+      lightMode: false,
+    },
+    {
+      shiftRu: "У",
+      shiftEn: "E",
+      valueRu: "у",
+      valueEn: "e",
+      lightMode: false,
+    },
+    {
+      shiftRu: "К",
+      shiftEn: "R",
+      valueRu: "к",
+      valueEn: "r",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Е",
+      shiftEn: "T",
+      valueRu: "е",
+      valueEn: "t",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Н",
+      shiftEn: "Y",
+      valueRu: "н",
+      valueEn: "y",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Г",
+      shiftEn: "U",
+      valueRu: "г",
+      valueEn: "u",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ш",
+      shiftEn: "I",
+      valueRu: "ш",
+      valueEn: "i",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Щ",
+      shiftEn: "O",
+      valueRu: "щ",
+      valueEn: "o",
+      lightMode: false,
+    },
+    {
+      shiftRu: "З",
+      shiftEn: "P",
+      valueRu: "з",
+      valueEn: "p",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Х",
+      shiftEn: "{",
+      valueRu: "х",
+      valueEn: "[",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ъ",
+      shiftEn: "}",
+      valueRu: "ъ",
+      valueEn: "]",
+      lightMode: false,
+    },
+    {
+      shiftRu: "/",
+      shiftEn: "|",
+      valueRu: "\\",
+      valueEn: "\\",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ф",
+      shiftEn: "A",
+      valueRu: "ф",
+      valueEn: "a",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ы",
+      shiftEn: "S",
+      valueRu: "ы",
+      valueEn: "s",
+      lightMode: false,
+    },
+    {
+      shiftRu: "В",
+      shiftEn: "D",
+      valueRu: "в",
+      valueEn: "d",
+      lightMode: false,
+    },
+    {
+      shiftRu: "А",
+      shiftEn: "F",
+      valueRu: "а",
+      valueEn: "f",
+      lightMode: false,
+    },
+    {
+      shiftRu: "П",
+      shiftEn: "G",
+      valueRu: "п",
+      valueEn: "g",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Р",
+      shiftEn: "H",
+      valueRu: "р",
+      valueEn: "h",
+      lightMode: false,
+    },
+    {
+      shiftRu: "О",
+      shiftEn: "J",
+      valueRu: "о",
+      valueEn: "j",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Л",
+      shiftEn: "K",
+      valueRu: "л",
+      valueEn: "k",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Д",
+      shiftEn: "L",
+      valueRu: "д",
+      valueEn: "l",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ж",
+      shiftEn: ":",
+      valueRu: "ж",
+      valueEn: ";",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Э",
+      shiftEn: '"',
+      valueRu: "э",
+      valueEn: "'",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Я",
+      shiftEn: "Z",
+      valueRu: "я",
+      valueEn: "z",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ч",
+      shiftEn: "X",
+      valueRu: "ч",
+      valueEn: "x",
+      lightMode: false,
+    },
+    {
+      shiftRu: "С",
+      shiftEn: "C",
+      valueRu: "с",
+      valueEn: "c",
+      lightMode: false,
+    },
+    {
+      shiftRu: "М",
+      shiftEn: "V",
+      valueRu: "м",
+      valueEn: "v",
+      lightMode: false,
+    },
+    {
+      shiftRu: "И",
+      shiftEn: "B",
+      valueRu: "и",
+      valueEn: "b",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Т",
+      shiftEn: "N",
+      valueRu: "т",
+      valueEn: "n",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ь",
+      shiftEn: "M",
+      valueRu: "ь",
+      valueEn: "m",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Б",
+      shiftEn: "<",
+      valueRu: "б",
+      valueEn: ",",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Ю",
+      shiftEn: ">",
+      valueRu: "ю",
+      valueEn: ".",
+      lightMode: false,
+    },
+    {
+      shiftRu: ",",
+      shiftEn: "?",
+      valueRu: ".",
+      valueEn: "/",
+      lightMode: false,
+    },
+    {
+      shiftRu: "&lArr;",
+      shiftEn: "&lArr;",
+      valueRu: "&lArr;",
+      valueEn: "&lArr;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "&uArr;",
+      shiftEn: "&uArr;",
+      valueRu: "&uArr;",
+      valueEn: "&uArr;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "&rArr;",
+      shiftEn: "&rArr;",
+      valueRu: "&rArr;",
+      valueEn: "&rArr;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Enter",
+      shiftEn: "Enter",
+      valueRu: "Enter",
+      valueEn: "Enter",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Cntrl",
+      shiftEn: "Cntrl",
+      valueRu: "Cntrl",
+      valueEn: "Cntrl",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Alt",
+      shiftEn: "Alt",
+      valueRu: "Alt",
+      valueEn: "Alt",
+      lightMode: false,
+    },
+    {
+      shiftRu: " ",
+      shiftEn: " ",
+      valueRu: " ",
+      valueEn: " ",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Tab",
+      shiftEn: "Tab",
+      valueRu: "Tab",
+      valueEn: "Tab",
+      lightMode: false,
+    },
+    {
+      shiftRu: "&dArr;",
+      shiftEn: "&dArr;",
+      valueRu: "&dArr;",
+      valueEn: "&dArr;",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Del",
+      shiftEn: "Del",
+      valueRu: "Del",
+      valueEn: "Del",
+      lightMode: false,
+    },
+    {
+      shiftRu: "Shift",
+      shiftEn: "Shift",
+      valueRu: "Shift",
+      valueEn: "Shift",
+      lightMode: false,
+    },
+  ],
+  shift: false,
+  language: "en",
+  sound: false,
+  helper: false,
+};
+
+// create elements
+
+if (localStorage.getItem("laguage")) {
+  data.language = localStorage.getItem("laguage");
+}
+
+console.log(data.language);
+
+const body = document.querySelector("body");
+
+const wrapper = document.createElement("div");
+wrapper.className = "wrapper";
+body.append(wrapper);
+
+const content = document.createElement("div");
+content.className = "content";
+wrapper.append(content);
+
+const textarea = document.createElement("textarea");
+textarea.className = "window";
+textarea.cols = "120";
+textarea.rows = "8";
+content.append(textarea);
+
+const buttonOff = document.createElement("button");
+buttonOff.className = "keybord__btn";
+buttonOff.innerHTML = "&#9000;";
+content.append(buttonOff);
+
+const wrapperKeybord = document.createElement("div");
+wrapperKeybord.className = "wrapper__keybord";
+content.append(wrapperKeybord);
+
+const keybord = document.createElement("div");
+keybord.className = "keybord";
+wrapperKeybord.append(keybord);
+
+// create keys
+
+data.keybordData.map((key) => {
+  const renderKey = document.createElement("div");
+  renderKey.className = "keybord__key";
+  keybord.append(renderKey);
+
+  const valueOne = document.createElement("span");
+  valueOne.className = "keybord__key--one";
+
+  if (data.language === "en") {
+    valueOne.innerHTML = key.valueEn;
+    renderKey.append(valueOne);
+  } else if (data.language === "ru") {
+    valueOne.innerHTML = key.valueRu;
+    renderKey.append(valueOne);
+  }
+
+  if (key.valueEn === "Alt") {
+    renderKey.style.width = "68px";
+  }
+  if (key.valueEn === "Cntrl") {
+    renderKey.style.width = "97px";
+  }
+
+  if (key.valueEn === "BackSpace") {
+    renderKey.style.width = "162px";
+  }
+  if (key.valueEn === " ") {
+    renderKey.style.width = "310px";
+  }
+  if (key.valueEn === "Enter") {
+    renderKey.style.width = "114px";
+  }
+  if (key.valueEn === "СapsLock") {
+    renderKey.style.width = "160px";
+  }
+  if (key.valueEn === "Shift") {
+    renderKey.style.width = "115px";
+  }
+  if (key.valueEn === "Helper &#128075;") {
+    renderKey.style.width = "164px";
+  }
+  if (key.valueEn === "Lang &#127758;") {
+    renderKey.style.width = "134px";
+  }
+  if (key.valueEn === "Sound &#127925;") {
+    renderKey.style.width = "150px";
+  }
+});
+
+// toggle keyboard
+
+const toggleButtonOff = () => {
+  wrapperKeybord.classList.toggle("keybord--off");
+  if (wrapperKeybord.classList.contains("keybord--off")) {
+    buttonOff.style.background = "#004258";
+  } else buttonOff.style.background = "#fee119";
+};
+
+buttonOff.addEventListener("click", toggleButtonOff);
+
+// text redactor
+
+const keys = document.querySelectorAll(".keybord__key");
+
+const makeText = (keys) => {
+  textarea.value = textarea.value + keys.childNodes[0].innerHTML;
+};
+
+const deleteText = () => {
+  textarea.value = textarea.value.substring(0, textarea.value.length - 1);
+};
+
+const capitalizeText = (keys) => {
+  textarea.value = textarea.value + keys.childNodes[0].innerHTML.toUpperCase();
+};
+
+const lowerText = (keys) => {
+  textarea.value = textarea.value + keys.childNodes[0].innerHTML.toLowerCase();
+};
+
+// change language
+
+const changeLanguageRus = (keys) => {
+  localStorage.setItem("laguage", "ru");
+  data.language = "ru";
+
+  keys.forEach((key, i) => {
+    key.childNodes[0].innerHTML = "";
+    key.childNodes[0].innerHTML = data.keybordData[i].valueRu;
+  });
+};
+
+const changeLanguageEn = (keys) => {
+  localStorage.setItem("laguage", "en");
+  data.language = "en";
+
+  keys.forEach((key, i) => {
+    key.childNodes[0].innerHTML = "";
+    key.childNodes[0].innerHTML = data.keybordData[i].valueEn;
+  });
+};
+
+// voice assistant
+
+let recognizer = new webkitSpeechRecognition();
+
+const speechRecognaizer = (lang) => {
+  recognizer.interimResults = true;
+  recognizer.continuous = true;
+  recognizer.lang = lang;
+
+  recognizer.onresult = function (event) {
+    let result = event.results[event.resultIndex];
+
+    if (result.isFinal) {
+      textarea.value = "";
+      textarea.value = textarea.value + result[0].transcript;
+    } else {
+      console.log("Промежуточный результат: ", result[0].transcript);
+    }
+  };
+
+  recognizer.start();
+};
+
+const createSpeech = (key) => {
+  key.classList.add("keybord__helper--active");
+
+  let lang;
+  if (data.language === "en") {
+    textarea.value = "speak into the microphone...";
+    lang = "en-En";
+    speechRecognaizer(lang);
+  } else if (data.language === "ru") {
+    textarea.value = "говорите в микрофон...";
+    lang = "ru-Ru";
+    speechRecognaizer(lang);
+  }
+};
+
+const removeSpeech = (key) => {
+  key.classList.remove("keybord__helper--active");
+  textarea.value = "";
+  recognizer.abort();
+};
+
+// general function
+
+const editText = (keys, key) => {
+  if (key.childNodes[0].innerHTML.includes("Язык")) {
+    changeLanguageEn(keys);
+  } else if (key.childNodes[0].innerHTML.includes("Lang")) {
+    changeLanguageRus(keys);
+  } else if (key.childNodes[0].innerHTML.includes("BackSpace")) {
+    deleteText();
+  } else if (
+    key.childNodes[0].innerHTML.includes("Help") ||
+    key.childNodes[0].innerHTML.includes("Помощь")
+  ) {
+    if (key.classList.contains("keybord__helper--active")) {
+      removeSpeech(key);
+    } else {
+      createSpeech(key);
+    }
+  } else {
+    makeText(key);
+  }
+};
+
+keys.forEach((k) => k.addEventListener("click", () => editText(keys, k)));
